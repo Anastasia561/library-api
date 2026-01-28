0dc1c4396b0fa72683f2414277ea6f6b78732013
Anastasia561 <kurulenkoanastasia@gmail.com>
2026-01-28 14:23:33 +0100

    ref: refactor dependencies

--- authorization-server/src/test/java/pl/edu/authorizationserver/AbstractIntegrationTest.java
+++ authorization-server/src/test/java/pl/edu/authorizationserver/AbstractIntegrationTest.java
@@ -2,11 +2,9 @@ package pl.edu.authorizationserver;
 
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.edu.authorizationserver.config.TestContainerConfig;
 
 @ActiveProfiles("test")
-@Testcontainers
 @Import(TestContainerConfig.class)
 public class AbstractIntegrationTest {
 }
